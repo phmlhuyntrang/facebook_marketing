8e2ff97a3ebef8bea0d4c05d5815d2220e6b1db1
Chiêu Trần <161787072+TranBaoChieu@users.noreply.github.com>
2024-08-01 12:43:32 +0700

    post to facebook

--- models/fb_marketing_post.py
+++ models/fb_marketing_post.py
@@ -14,25 +14,34 @@ class MarketingPost(models.Model):
 
     def post_blog_to_facebook(self):
         for record in self:
-            blog_content = record.blog.name  # Assuming 'name' contains the blog content
-            blog_id = record.blog.id
+            # Assuming 'name', 'content', 'author', and 'date_published' are fields in 'blog.post'
+            blog_name = record.blog.name
+            blog_content = record.blog.content
+            blog_author = record.blog.author_id.name  # Assuming 'author_id' is a Many2one field to 'res.partner'
+
+            # Format the content to post on Facebook
+            post_content = (
+                f"Blog Title: {blog_name}\n"
+                f"Content: {blog_content}\n"
+                f"Author: {blog_author}\n"
+            )
 
             for page in record.page:
                 page_id = page.page_id
                 access_token = page.access_token
 
                 # Logic to post the blog to Facebook
-                _logger.info(f"Posting blog '{blog_content}' (ID: {blog_id}) to Facebook page '{page_id}' using access token '{access_token}'")
+                _logger.info(f"Posting blog '{blog_name}' (ID: {record.blog.id}) to Facebook page '{page_id}' using access token '{access_token}'")
                 
-                # Example API call (this is just a placeholder and needs to be replaced with actual API call)
-                response = requests.post(
-                    f'https://graph.facebook.com/{page_id}/feed',
-                    data={
-                        'message': blog_content,
-                        'access_token': access_token
-                    }
-                )
-                if response.status_code == 200:
+                try:
+                    response = requests.post(
+                        f'https://graph.facebook.com/{page_id}/feed',
+                        data={
+                            'message': post_content,
+                            'access_token': access_token
+                        }
+                    )
+                    response.raise_for_status()  # Raise an exception for HTTP errors
                     _logger.info(f"Successfully posted to page '{page_id}'")
-                else:
-                    _logger.error(f"Failed to post to page '{page_id}': {response.text}")
+                except requests.exceptions.RequestException as e:
+                    _logger.error(f"Failed to post to page '{page_id}': {e}")
